b3270ce733daa8f37257d38916ddfc940ef9c621
Eka Prasetia <ekaone3033@gmail.com>
2020-06-07 22:15:22 +0700

    Updates

---
+++ precache-manifest.d09a7bd881e61d367b57993294b8d4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7399bd88e8c9e09a581820609799bcf",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "5384e14e4935fab953d2",
+    "url": "/Expense-Tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5384e14e4935fab953d2",
+    "url": "/Expense-Tracker/static/js/2.6f88cc9d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Expense-Tracker/static/js/2.6f88cc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7d8dae2fe125a05792",
+    "url": "/Expense-Tracker/static/js/main.2b77048c.chunk.js"
+  },
+  {
+    "revision": "b28ece1ade5ee2f3f655",
+    "url": "/Expense-Tracker/static/js/runtime-main.f60d1b5d.js"
+  }
+]);
